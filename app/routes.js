6e2a893d66da3e5e599ff285ff720f45605c73f8
qdumont <quadebdumont@gmail.com>
2015-09-26 16:00:10 -0700

    working app

---
+++ app/routes.js
@@ -0,0 +1,93 @@
+module.exports = function(app, passport) {
+
+  // normal routes ===============================================================
+
+  // show the home page (will also have our login links)
+  app.get('/', function(req, res) {
+    res.render('index.ejs');
+  });
+
+  // PROFILE SECTION =========================
+  app.get('/profile', isLoggedIn, function(req, res) {
+    res.render('profile.ejs', {
+      user : req.user
+    });
+  });
+
+  // LOGOUT ==============================
+  app.get('/logout', function(req, res) {
+    req.logout();
+    res.redirect('/');
+  });
+
+
+  // locally --------------------------------
+  // LOGIN ===============================
+  // show the login form
+  app.get('/login', function(req, res) {
+    res.render('login.ejs', { message: req.flash('loginMessage') });
+  });
+
+  // process the login form
+  app.post('/login', passport.authenticate('local-login', {
+    successRedirect : '/profile', // redirect to the secure profile section
+    failureRedirect : '/login', // redirect back to the signup page if there is an error
+    failureFlash : true // allow flash messages
+  }));
+
+  // SIGNUP =================================
+  // show the signup form
+  app.get('/signup', function(req, res) {
+    res.render('signup.ejs', { message: req.flash('signupMessage') });
+  });
+
+  app.post('/transactions', function(req, res){
+    var obj = {
+        amount: req.body.dollars,
+        description: req.body.description,
+        date: Date.now()
+      }
+      console.log(req.body.dollars);
+      console.log(req.user.local.balance);
+      req.user.local.balance = parseFloat(req.user.local.balance) + parseFloat(req.body.dollars);
+      req.user.local.transactions.push(obj);
+      req.user.save();
+      res.redirect('/profile')
+  })
+  // process the signup form
+  app.post('/signup', passport.authenticate('local-signup', {
+    successRedirect : '/profile', // redirect to the secure profile section
+    failureRedirect : '/signup', // redirect back to the signup page if there is an error
+    failureFlash : true // allow flash messages
+  }));
+
+
+  // =============================================================================
+  // UNLINK ACCOUNTS =============================================================
+  // =============================================================================
+  // used to unlink accounts. for social accounts, just remove the token
+  // for local account, remove email and password
+  // user account will stay active in case they want to reconnect in the future
+
+  // local -----------------------------------
+  app.get('/unlink/local', isLoggedIn, function(req, res) {
+    var user            = req.user;
+    user.local.fullName = undefined;
+    user.local.email    = undefined;
+    user.local.password = undefined;
+    user.save(function(err) {
+      res.redirect('/profile');
+    });
+  });
+
+
+  // route middleware to ensure user is logged in
+  function isLoggedIn(req, res, next) {
+    if (req.isAuthenticated()){
+      return next();
+
+    }
+
+    res.redirect('/');
+  }
+}
